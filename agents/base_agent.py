620fe375748aec4269c16a4ef243aa0379318437
Arya Nandankar <captainlevi140@gmail.com>
2026-04-24 23:03:30 +0530

    fix final gemini model

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -12,7 +12,7 @@ class BaseAgent:
             raise RuntimeError("❌ GEMINI_API_KEY not set")
 
         genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel("gemini-pro")
+       self.model = genai.GenerativeModel("gemini-1.5-flash-8b")
 
     def _chat(self, messages):
         prompt = ""
